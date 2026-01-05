8578bcf89033bfb21c25b24053e01fc596300cf9
lorne <1991wangliang@gmail.com>
2026-01-05 14:39:41 +0800

    init

---
+++ apps/ui-design/rsbuild.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import * as path from "path";
+
+// Docs: https://rsbuild.rs/config/
+export default defineConfig({
+  plugins: [pluginReact()],
+
+  resolve: {
+    alias: { '@': path.resolve(__dirname, 'src') },
+  },
+});
